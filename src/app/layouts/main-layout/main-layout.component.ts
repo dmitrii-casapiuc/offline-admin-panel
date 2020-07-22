7fd1a9871060278a07a3f9f2bcb233900aa5caa0
Dmitrii Casapciuc <dmitrii.casapciuc@feel-it-services.com>
2020-07-22 21:32:02 +0300

    Added structure main layout

--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+  styleUrls: ['./main-layout.component.scss'],
 })
-export class MainLayoutComponent implements OnInit {
+export class MainLayoutComponent {
 
-  constructor() { }
+  desktopQuery$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    )
 
-  ngOnInit(): void {
-  }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
 }
--- src/app/layouts/main-layout/main-layout.module.ts
+++ src/app/layouts/main-layout/main-layout.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
 
-import { MainLayoutComponent } from './main-layout.component'
+import { MaterialModule } from '@app/material.module'
 import { MainLayoutRoutingModule } from './main-layout-routing.module'
+import { MainLayoutComponent } from './main-layout.component'
+import { SidenavComponent } from './sidenav/sidenav.component'
+import { ToolbarComponent } from './toolbar/toolbar.component'
 
 @NgModule({
-  declarations: [MainLayoutComponent],
+  declarations: [
+    MainLayoutComponent,
+    SidenavComponent,
+    ToolbarComponent
+  ],
   imports: [
     CommonModule,
-    MainLayoutRoutingModule
+    MainLayoutRoutingModule,
+    MaterialModule
   ],
   exports: [RouterModule]
 })
