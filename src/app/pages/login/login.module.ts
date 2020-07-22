33681c2c47c20f6987f0d81b7fd9a91bb75dc17f
Dima Casapciuc <dmitrii.casapciuc@feel-it-services.com>
2020-07-22 10:28:29 +0300

    Added html for login

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
+import { MaterialModule } from '@app/material.module'
 import { LoginComponent } from './login.component'
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([{ path: '', component: LoginComponent, pathMatch: 'full' }])
   ],
 })
