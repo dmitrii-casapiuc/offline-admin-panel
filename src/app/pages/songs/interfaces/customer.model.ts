c5dcda751cbac80bece7292ca2e092cc70dc5bdd
Dmitrii Casapciuc <dmitrii.casapciuc@feel-it-services.com>
2020-07-27 19:20:42 +0300

    WIP with songs table

--- src/app/pages/songs/interfaces/customer.model.ts
+++ src/app/pages/songs/interfaces/customer.model.ts
@@ -1,45 +1,45 @@
 export class Customer {
-  id: number;
-  imageSrc: string;
-  firstName: string;
-  lastName: string;
-  street: string;
-  zipcode: number;
-  city: string;
-  phoneNumber: string;
-  mail: string;
+  id: number
+  imageSrc: string
+  firstName: string
+  lastName: string
+  street: string
+  zipcode: number
+  city: string
+  phoneNumber: string
+  mail: string
 
   constructor(customer) {
-    this.id = customer.id;
-    this.imageSrc = customer.imageSrc;
-    this.firstName = customer.firstName;
-    this.lastName = customer.lastName;
-    this.street = customer.street;
-    this.zipcode = customer.zipcode;
-    this.city = customer.city;
-    this.phoneNumber = customer.phoneNumber;
-    this.mail = customer.mail;
+    this.id = customer.id
+    this.imageSrc = customer.imageSrc
+    this.firstName = customer.firstName
+    this.lastName = customer.lastName
+    this.street = customer.street
+    this.zipcode = customer.zipcode
+    this.city = customer.city
+    this.phoneNumber = customer.phoneNumber
+    this.mail = customer.mail
   }
 
   get name() {
-    let name = '';
+    let name = ''
 
     if (this.firstName && this.lastName) {
-      name = this.firstName + ' ' + this.lastName;
+      name = this.firstName + ' ' + this.lastName
     } else if (this.firstName) {
-      name = this.firstName;
+      name = this.firstName
     } else if (this.lastName) {
-      name = this.lastName;
+      name = this.lastName
     }
 
-    return name;
+    return name
   }
 
   set name(value) {
   }
 
   get address() {
-    return `${this.street}, ${this.zipcode} ${this.city}`;
+    return `${this.street}, ${this.zipcode} ${this.city}`
   }
 
   set address(value) {
