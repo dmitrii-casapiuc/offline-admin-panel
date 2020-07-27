c872b06f2f340db27347696ada36db5c171f460b
Dima Casapciuc <dmitrii.casapciuc@feel-it-services.com>
2020-07-27 12:10:27 +0300

    WIP with page songs

--- src/app/pages/songs/songs.module.ts
+++ src/app/pages/songs/songs.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core'
 
-import { SongsComponent } from './songs.component'
 import { RouterModule } from '@angular/router'
+import { CommonModule } from '@angular/common'
+import { IconModule } from '@visurel/iconify-angular'
+import { FlexLayoutModule } from '@angular/flex-layout'
+
+import { MaterialModule } from '@app/material.module'
+import { SongsComponent } from './songs.component'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
-  declarations: [SongsComponent],
-  imports: [RouterModule.forChild([{ path: '', component: SongsComponent }])]
+  declarations: [
+    SongsComponent
+  ],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MaterialModule,
+    IconModule,
+    FlexLayoutModule,
+    RouterModule.forChild([{ path: '', component: SongsComponent }])
+  ]
 })
 export class SongsModule {}
